d29254e7dee9e5777aedac27df1b173e292d9237
Marcelina <86987@student.ue.poznan.pl>
2022-05-28 09:09:30 +0200

    6 podejście

--- main.py
+++ main.py
@@ -23,3 +23,29 @@ class Pracownik:
     def __obliczanie_skladki__(self):
         self.skladki = round(self.wynagrodzenie *0.0976, 2) + round(self.wynagrodzenie*0.065, 2) + round(self.wynagrodzenie*0.0193,2) + round(self.wynagrodzenie*0.0245,2) + round(self.wynagrodzenie*0.001,2)
         return round(self.skladki,2)
+
+    def __koszt__(self):
+        self.koszt = round(self.wynagrodzenie + self.skladki, 2)
+        return self.koszt
+    def __razem__(self):
+        return round(self.wynagrodzenie + self.__obliczanie_skladki__(),2)      
+
+wszyscy_pracownicy = int(input())
+pracownicy = []
+for n in range(wszyscy_pracownicy):
+    imie_wynagrodzenie = input().split()
+    imie = imie_wynagrodzenie[0]
+    wynagrodzenie = imie_wynagrodzenie[1]
+    pracownik_obj = Pracownik(imie, wynagrodzenie)
+    pracownicy.append(pracownik_obj)
+
+laczny_koszt_pracodawcy = 0 
+
+for m in range(wszyscy_pracownicy):
+    laczny_koszt_pracodawcy += pracownicy[m].__razem__()
+    imie = pracownicy[m].imie
+    wynagrodzenie = pracownicy[m].wynagrodzenie
+    print(imie, f"{pracownicy[m].__wynagrodzenie_netto__():.2f}", f"{pracownicy[m].__obliczanie_skladki__():.2f}", f"{pracownicy[m].__koszt__():.2f}")
+
+print(laczny_koszt_pracodawcy)
+
